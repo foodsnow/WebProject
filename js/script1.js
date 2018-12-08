5b50178c99cc46d92f880e3fd0d84f01c4a0b082
foodsnow <dut.kht@gmail.com>
2018-12-08 14:49:21 -0800

    with slider;

--- js/script1.js
+++ js/script1.js
@@ -1,18 +1,35 @@
 var slides = document.querySelector(".main-wrapper .main .slider ul");
 var slide = document.querySelectorAll(".slider ul li");
-var len = slide[0].offsetWidth * slide.length;
+var len = slide.length;
+
+let leftButton = document.querySelector(".main-wrapper .main .slider button#left");
+let rightButton = document.querySelector(".main-wrapper .main .slider button#right");
 
 let trans = 0;
+let current = 0;
 
-function doSlide(){
-	if (trans + 8 * slide[0].offsetWidth <= len + slide[0].offsetWidth/2 &&
-		trans + 8 * slide[0].offsetWidth >= len - slide[0].offsetWidth/2) {
+function swipeRight(){
+	if (current + 7 >= len) {
 		trans = 0;
-	}
-	else{
+		current = 0;
+	}else{
 		trans += slide[0].offsetWidth;
+		current += 1;
+	}
+	slides.style.transform = "translateX(" + -trans + "px)";
+}
+function swipeLeft(){
+	if (current <= 0) {
+		trans = slide[0].offsetWidth * (len - 7);
+		current = len - 7;
+	}else{
+		trans -= slide[0].offsetWidth;
+		current -= 1;
 	}
 	slides.style.transform = "translateX(" + -trans + "px)";
 }
 
-setInterval(doSlide, 8000);
+leftButton.addEventListener("click", swipeLeft);
+rightButton.addEventListener("click", swipeRight);
+setInterval(swipeRight, 8000);
+
